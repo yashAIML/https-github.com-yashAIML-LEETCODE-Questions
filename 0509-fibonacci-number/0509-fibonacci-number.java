27b0140f37c493f73c7443e19b03c18b03c1e426
Yash Raundal <yraundal03@gmail.com>
2026-07-14 23:01:22 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.25%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n ==0) return 0;
+        if(n== 1) return 1;
+        int n1 = 0;
+        int n2 = 1;
+        for(int i =2; i<=n;i++){
+            int fib = n1+n2;
+            n1 = n2;
+            n2 = fib;
+        }
+        return n2;
+    }
+}
